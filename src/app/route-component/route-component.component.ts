f045041f5b43490f692b52ff365cc141c0c0da77
Sergei Gusev <Sergei_Gusev1@epam.com>
2019-06-24 12:07:13 +0300

    add default route component

---
+++ src/app/route-component/route-component.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-route-component',
+  templateUrl: './route-component.component.html',
+})
+export class RouteComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
