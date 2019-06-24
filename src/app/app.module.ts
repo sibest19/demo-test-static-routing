4f8000ffee8d73b6b012d6a0419685877babc5e4
Sergei Gusev <Sergei_Gusev1@epam.com>
2019-06-24 12:09:05 +0300

    steps to reproduce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouteComponentComponent } from './route-component/route-component.component';
+
+import { RouterModule } from '@angular/router';
+import Constants from './constants';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+     AppComponent,
+     RouteComponentComponent,
   ],
   imports: [
-    BrowserModule
+     BrowserModule,
+     RouterModule.forRoot([
+       { path: '', pathMatch: 'full', redirectTo: Constants.defaultUrl },
+       { path: Constants.defaultUrl, component: RouteComponentComponent },
+     ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
